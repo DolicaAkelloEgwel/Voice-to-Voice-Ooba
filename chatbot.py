8501caad721016b4cfaa604888e31e4532fd5131
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-11-04 12:14:59 +0000

    some tidying

--- chatbot.py
+++ chatbot.py
@@ -1,50 +1,46 @@
 from pickle import TRUE
-import re
-import json
 import time
 import requests
-import traceback
 import aispeech
 import wave
 
 import transcribe
+
 message_log = []
-AI_RESPONSE_FILENAME = 'ai-response.txt'
+AI_RESPONSE_FILENAME = "ai-response.txt"
 logging_eventhandlers = []
-PORT = 5000
+PORT = 7860
 history = []
+
 
 def send_user_input(user_input):
     global message_log
     print(message_log)
     log_message(f"User: {user_input}")
     message_log.append({"role": "user", "content": user_input})
-    url = f'http://0.0.0.0:{PORT}/v1/chat/completions'
+    url = f"http://127.0.0.1:{PORT}/v1/chat/completions"
     headers = {"Content-Type": "application/json"}
     json = {
-            "messages": [
-                {"role": "system", "content": f"You are an ai assistant"},
-                {"role": "user", "content": f"{user_input}"}
-            ],
-            "history": f"{history}",
-            "stop": ["### Instruction:"],
-            "temperature": 0.7,
-            "max_tokens": 800,
-            "stream": False
-        }
-    
+        "messages": [
+            {"role": "system", "content": f"You are an ai assistant"},
+            {"role": "user", "content": f"{user_input}"},
+        ],
+        "history": f"{history}",
+        "stop": ["### Instruction:"],
+        "temperature": 0.7,
+        "max_tokens": 800,
+        "stream": False,
+    }
+
     response = requests.post(url, headers=headers, json=json)
     if response.status_code == 200:
-        result = response.json()['choices'][0]['message']['content']
-        history.append({"role": "system", "content": result } )
-        history.append({"role": "user", "content": user_input } )
+        result = response.json()["choices"][0]["message"]["content"]
+        history.append({"role": "system", "content": result})
+        history.append({"role": "user", "content": user_input})
         text = result
-        log_message(f'{text}')
+        log_message(f"{text}")
         aispeech.initialize(text)
         time.sleep(0.1)
-        
-                    
-        
 
 
 def log_message(message_text):
--- transcribe.py
+++ transcribe.py
@@ -1,29 +1,29 @@
 from pickle import TRUE
-import re
 import speech_recognition as sr
-import pyttsx3
-import json
-import requests
-import traceback
 import chatbot
-import wave
-import pyaudio
 import whisper
 from threading import Thread
 
-whisper_filter_list = ['you', 'thank you.',
-                       'thanks for watching.', "Thank you for watching."]
+whisper_filter_list = [
+    "you",
+    "thank you.",
+    "thanks for watching.",
+    "Thank you for watching.",
+]
 
 MIC_OUTPUT_FILENAME = "outfile.wav"
 VOICE_OUTPUT_FILENAME = "audioResponse.wav"
 
 logging_eventhandlers = []
 
+
 def initialize_model():
     global model
     model = whisper.load_model("tiny")
-    
+
+
 auto_recording = False
+
 
 def start_record_auto():
     global auto_recording
@@ -31,19 +31,23 @@ def start_record_auto():
     thread = Thread(target=start_STTS_loop_chat)
     thread.start()
 
+
 def start_STTS_loop_chat():
     global auto_recording
     while auto_recording:
         listen()
+
 
 def stop_record_auto():
     global auto_recording, ambience_adjusted
     auto_recording = False
     ambience_adjusted = False
 
+
 def listen():
     r = sr.Recognizer()
-    mic = sr.Microphone()
+    mic = sr.Microphone(device_index=9)
+    print("Mic:", mic)
     with mic as source:
         r.adjust_for_ambient_noise(source, duration=0.2)
         print("Speak now")
@@ -56,25 +60,28 @@ def listen():
         audio = whisper.load_audio(MIC_OUTPUT_FILENAME)
         audio = whisper.pad_or_trim(audio)
         mel = whisper.log_mel_spectrogram(audio).to(model.device)
-        options = whisper.DecodingOptions(task='transcribe',
-                                          language='english', without_timestamps=True, fp16=False if model.device == 'cpu' else None)
+        options = whisper.DecodingOptions(
+            task="transcribe",
+            language="english",
+            without_timestamps=True,
+            fp16=False if model.device == "cpu" else None,
+        )
         result = whisper.decode(model, mel, options)
         user_input = result.text
         global whisper_filter_list
-        if (user_input == ''):
+        if user_input == "":
             return
-        print(f'filtering')
-        if (user_input.strip().lower() in whisper_filter_list):
-            print(f'Input filtered.')
+        print(f"filtering")
+        if user_input.strip().lower() in whisper_filter_list:
+            print(f"Input filtered.")
             return
-        
+
         chatbot.send_user_input(user_input)
         return
-        
+
+
 def log_message(message_text):
     print(message_text)
     global logging_eventhandlers
     for eventhandler in logging_eventhandlers:
         eventhandler(message_text)
-
-        
